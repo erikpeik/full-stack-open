ca066b9eecad221fbcf0872dc2d72733f8eb1d44
Erik Mende <emende@student.hive.fi>
2022-09-04 20:29:54 +0300

    Exercise 2.15 return to phonebook application
    Saving added numbers to backend server

--- part2/photobook/src/App.js
+++ part2/photobook/src/App.js
@@ -51,7 +51,9 @@ const App = () => {
 		if (names.includes(newName)) {
 			alert(`${newName} is already added to phonebook`)
 		} else  {
-			setPersons(persons.concat(personObject))
+			axios
+				.post("http://localhost:3001/persons", personObject)
+				.then(response => setPersons(persons.concat(response.data)))
 		}
 		setNewName('')
 		setNewNumber('')
