e1ad37ff8a517016f172a1114f4f81e70edbf7f4
Erik Mende <emende@student.hive.fi>
2022-09-07 17:46:27 +0300

    Exercise 7.4 simplified anecdote creation form with the custom hook useField

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useField } from './hooks'
+
 import {
 	BrowserRouter as Router,
 	Routes, Route, Link, useParams,
@@ -54,19 +56,19 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-	const [content, setContent] = useState('')
-	const [author, setAuthor] = useState('')
-	const [info, setInfo] = useState('')
+	const content = useField('text')
+	const author = useField('text')
+	const info = useField('text')
 
 	const navigate = useNavigate()
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
 		props.addNew({
-			content,
-			author,
-			info,
-			votes: 0
+			"content": content.value,
+			"author": author.value,
+			"info": info.value,
+			"votes": 0
 		})
 		props.setNotification(`a new anecdote ${content} created!`)
 		setTimeout(() => {
@@ -81,15 +83,15 @@ const CreateNew = (props) => {
 			<form onSubmit={handleSubmit}>
 				<div>
 					content
-					<input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+					<input {...content} />
 				</div>
 				<div>
 					author
-					<input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+					<input {...author} />
 				</div>
 				<div>
 					url for more info
-					<input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+					<input {...info} />
 				</div>
 				<button>create</button>
 			</form>
