d071cecf9726b4c759e9e663f858a467899b5298
Erik Mende <emende@student.hive.fi>
2022-11-21 18:02:24 +0200

    Starting doing part4

---
+++ part4/app.js
@@ -0,0 +1,132 @@
+const express = require('express')
+const app = express()
+const Person = require('./models/person')
+var morgan = require('morgan')
+const cors = require('cors')
+const logger = require('./utils/logger')
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+      JSON.stringify(req.body),
+    ].join(' ')
+  })
+)
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then((notes) => {
+    res.json(notes)
+  })
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((note) => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+app.post('/api/persons', (req, res, next) => {
+  const { name, number } = req.body
+
+  Person.findOne({ name }).then((person) => {
+    if (person) {
+      return res.status(400).json({ error: 'name must be unique' })
+    }
+  })
+
+  const person = new Person({
+    name,
+    number,
+  })
+
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson)
+    })
+    .catch((error) => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
+  const id = req.params.id
+
+  const person = {
+    name,
+    number,
+  }
+
+  Person.findByIdAndUpdate(id, person, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        return res.json(updatedPerson)
+      } else {
+        return res.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
+})
+
+app.get('/info', (req, res) => {
+  Person.find({}).then((notes) => {
+    res.send(
+      `<p>Phonebook has info for ${notes.length} people</p> <p>${Date()}</p>`
+    )
+  })
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+module.exports = app
---
+++ part4/models/person.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+
+const url = config.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: [3, 'Name must be at least 3 characters long'],
+    required: [true, 'Name is required'],
+  },
+  number: {
+    type: String,
+    minLength: [8, 'Number must be at least 8 characters long'],
+    required: [true, 'Number number is required'],
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-{0,1}\d{5,}$/.test(v)
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Person', noteSchema)
