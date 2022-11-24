c544d813cea8e83b10e0336dc904f14da7303c4b
Erik Mende <emende@student.hive.fi>
2022-11-24 12:55:08 +0200

    4.4: totalLikes function with tests

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,14 @@
 const dummy = (blogs) => {
+  !blogs
   return 1
+}
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => (sum + item.likes)
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
 }
 
 module.exports = {
   dummy,
+  totalLikes
 }
