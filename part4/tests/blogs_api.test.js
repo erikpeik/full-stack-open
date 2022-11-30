73be6d84e972f4b1addef576af35393bfc175bc2
Erik Mende <emende@student.hive.fi>
2022-11-30 17:22:59 +0200

    4.9*: Blog list tests, step2
    Verifying the unique identifier property of the blog posts is named id, by default the database names the property _id

--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -26,6 +26,11 @@ test('all notes are returned', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('Verifying the unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
+
 // test('a specific note is within the returned notes', async () => {
 //   const response = await api.get('/api/blogs')
 
