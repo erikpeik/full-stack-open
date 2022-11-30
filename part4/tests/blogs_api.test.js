63e7f1aa0f563c671ae3233dcc2325457d844920
Erik Mende <emende@student.hive.fi>
2022-11-30 17:26:46 +0200

    4.10: Blog list tests, step3
    Test that verifies that making an HTTP POST request to /api/blogs url successfully creates a new blog post.

--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -14,14 +14,14 @@ beforeEach(async () => {
   }
 })
 
-test('notes are returned as json', async () => {
+test('Notes are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 }, 100000)
 
-test('all notes are returned', async () => {
+test('All notes are returned', async () => {
   const response = await api.get('/api/blogs')
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
@@ -31,32 +31,32 @@ test('Verifying the unique identifier property of the blog posts is named id', a
   expect(response.body[0].id).toBeDefined()
 })
 
+test('Making an HTTP POST request to /api/blogs successfully creates a new blog post', async () => {
+  const newBlog = {
+    title: 'How to solve the Rubik\'s Cube?',
+    author: 'Denes Ferenc',
+    url: 'https://ruwix.com/the-rubiks-cube/how-to-solve-the-rubiks-cube-beginners-method/',
+    likes: 0,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const contents = response.body.map((r) => r.title)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(contents).toContain('How to solve the Rubik\'s Cube?')
+})
+
 // test('a specific note is within the returned notes', async () => {
 //   const response = await api.get('/api/blogs')
 
 //   const contents = response.body.map((r) => r.title)
 //   expect(contents).toContain('Canonical string reduction')
-// })
-
-// test('a valid note can be added', async () => {
-//   const newBlog = {
-//     title: 'How to solve the Rubik\'s Cube?',
-//     author: 'Denes Ferenc',
-//     url: 'https://ruwix.com/the-rubiks-cube/how-to-solve-the-rubiks-cube-beginners-method/',
-//     likes: 0,
-//   }
-
-//   await api
-//     .post('/api/blogs')
-//     .send(newBlog)
-//     .expect(201)
-//     .expect('Content-Type', /application\/json/)
-
-//   const response = await api.get('/api/blogs')
-//   const contents = response.body.map((r) => r.title)
-
-//   expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
-//   expect(contents).toContain('How to solve the Rubik\'s Cube?')
 // })
 
 // test('empty note is not added', async () => {
