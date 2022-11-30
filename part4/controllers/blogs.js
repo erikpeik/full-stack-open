ed240f138574b525b3f4351a0d15254f3865659a
Erik Mende <emende@student.hive.fi>
2022-11-30 23:09:59 +0200

    Changed then methods to async/await functions

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,22 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result)
-    })
-    .catch((error) => {
-      response.status(400).json({ error: error.message })
-    })
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
@@ -25,11 +18,11 @@ blogsRouter.delete('/:id', async (request, response) => {
 })
 
 blogsRouter.put('/:id', async (request, response) => {
+  const id = request.params.id
   const { title, author, url, likes } = request.body
+
   const blog = { title, author, url, likes }
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
-    new: true,
-  })
+  const updatedBlog = await Blog.findByIdAndUpdate(id, blog, { new: true })
   response.json(updatedBlog)
 })
 
